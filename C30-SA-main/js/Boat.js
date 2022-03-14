757233420ea3a470ac053d854ba083924ba0ce25
Dylanp815 <83504343+Dylanp815@users.noreply.github.com>
2022-03-13 21:08:17 -0500

    Add files via upload

---
+++ C30-SA-main/js/Boat.js
@@ -0,0 +1,25 @@
+class Boat {
+
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    World.add(world, this.body);
+    this.image = loadImage("./assets/boat.png");
+  }
+  display() {
+    var pos = this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    fill("brown");
+    //rectMode(CENTER);
+    //rect(0, 0, this.width, this.height);
+    image(this.image, 0, 0, this.width,this.height)
+    pop();
+    
+  }
+}
+
